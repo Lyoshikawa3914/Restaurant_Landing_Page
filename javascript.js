3b55843df3d97c20604d8b3755f9d9a247e77fef
Lawrence Yoshikawa <Lyoshikawa3914@gmail.com>
2023-07-30 06:53:33 -0700

    added javascript effects to change the hamburger menuwhenever it is clicked

--- javascript.js
+++ javascript.js
@@ -1,10 +1,23 @@
 const bar1 = document.querySelector('#bar1');
 const bar2 = document.querySelector('#bar2');
 const bar3 = document.querySelector('#bar3');
-
 // bar3.style.cssText = 'transform: translate(0, -300%) rotate(45deg)';
 // bar1.style.cssText = 'transform: translate(0, 300%) rotate(135deg)';
 // bar2.style.cssText = 'visibility: hidden';
+
+const menuButton = document.querySelector('.hamburger');
+
+menuButton.addEventListener('click', function() {
+    menuButton.classList.toggle('isActive');
+    bar2.classList.toggle('isActive');
+    bar3.classList.toggle('isActive');
+});
+
+
+
+
+
+
 
 
 
