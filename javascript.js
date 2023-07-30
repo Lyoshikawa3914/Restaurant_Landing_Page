c5e7fa3b9ae1b031f244a0800dd7e9f467832bf2
Lawrence Yoshikawa <Lyoshikawa3914@gmail.com>
2023-07-30 08:11:36 -0700

    added a toggle functionality to that will have the nav menu appear and disappear when pressing hamburger button

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
 const bar1 = document.querySelector('#bar1');
 const bar2 = document.querySelector('#bar2');
 const bar3 = document.querySelector('#bar3');
+
+const navMenu = document.querySelector('.navContainer');
+
 // bar3.style.cssText = 'transform: translate(0, -300%) rotate(45deg)';
 // bar1.style.cssText = 'transform: translate(0, 300%) rotate(135deg)';
 // bar2.style.cssText = 'visibility: hidden';
@@ -11,6 +14,7 @@ menuButton.addEventListener('click', function() {
     menuButton.classList.toggle('isActive');
     bar2.classList.toggle('isActive');
     bar3.classList.toggle('isActive');
+    navMenu.classList.toggle('isActive');
 });
 
 
