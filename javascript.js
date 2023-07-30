c213f5937a3d349532e4557011d0d4e1c6f37c9b
Lawrence Yoshikawa <Lyoshikawa3914@gmail.com>
2023-07-30 08:45:56 -0700

    added more stuff

--- javascript.js
+++ javascript.js
@@ -2,7 +2,11 @@ const bar1 = document.querySelector('#bar1');
 const bar2 = document.querySelector('#bar2');
 const bar3 = document.querySelector('#bar3');
 
+const body = document.querySelector('body');
 const navMenu = document.querySelector('.navContainer');
+const heroContainer = document.querySelector('.heroContainer');
+const menuContainer = document.querySelector('.menuContainer');
+const footer = document.querySelector('footer');
 
 // bar3.style.cssText = 'transform: translate(0, -300%) rotate(45deg)';
 // bar1.style.cssText = 'transform: translate(0, 300%) rotate(135deg)';
@@ -15,6 +19,8 @@ menuButton.addEventListener('click', function() {
     bar2.classList.toggle('isActive');
     bar3.classList.toggle('isActive');
     navMenu.classList.toggle('isActive');
+
+    body.classList.toggle('isActive');
 });
 
 
