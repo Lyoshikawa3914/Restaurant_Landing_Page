855e5a0e09b927df08d1d71b0cc5edb130dd6129
Lawrence Yoshikawa <Lyoshikawa3914@gmail.com>
2023-07-29 20:41:40 -0700

    played around with the hamburger menu

---
+++ javascript.js
@@ -0,0 +1,10 @@
+const bar1 = document.querySelector('#bar1');
+const bar2 = document.querySelector('#bar2');
+const bar3 = document.querySelector('#bar3');
+
+// bar3.style.cssText = 'transform: translate(0, -300%) rotate(45deg)';
+// bar1.style.cssText = 'transform: translate(0, 300%) rotate(135deg)';
+// bar2.style.cssText = 'visibility: hidden';
+
+
+
