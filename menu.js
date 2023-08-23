acca9bb7e4cb44863e0d1c2c631c75005187bd2d
Lawrence Yoshikawa <Lyoshikawa3914@gmail.com>
2023-08-23 14:46:57 -0700

    added toggle of hamburger button when user clicks on food items in menu section

--- menu.js
+++ menu.js
@@ -1,14 +1,21 @@
+const theBody = document.querySelector('body');
+
 const sandwhiches = document.querySelector(".sandwiches");
 const drinks = document.querySelector(".drinks");
 const sides = document.querySelector(".sides");
 const desserts = document.querySelector(".desserts");
+
 const foodContainerTitle = document.querySelector(".foodContainerTitle");
 const closeButton = document.querySelector(".close");
+
+const theHamburgerButton = document.querySelector(".hamburger");
 
 const foodContainer = document.querySelector(".foodContainer");
 
 closeButton.addEventListener('click', function() {
     foodContainer.style.left = "-1000%";
+    theBody.classList.toggle('isActive');
+    theHamburgerButton.disabled = false;
 })
 
 sandwhiches.addEventListener('click', function() {
@@ -16,22 +23,31 @@ sandwhiches.addEventListener('click', function() {
 
     foodContainer.style.left =  '5%'
     foodContainerTitle.textContent = "Sandwiches";
+    theBody.classList.toggle("isActive")
+    theHamburgerButton.disabled = true;
+
 });
 drinks.addEventListener('click', function() {
     console.log('dranks');
 
     foodContainer.style.left =  '5%'
     foodContainerTitle.textContent = "Drinks";
+    theBody.classList.toggle("isActive")
+    theHamburgerButton.disabled = true;
 });
 sides.addEventListener('click', function() {
     console.log('sides');
 
     foodContainer.style.left =  '5%'
     foodContainerTitle.textContent = "Sides";
+    theBody.classList.toggle("isActive")
+    theHamburgerButton.disabled = true;
 });
 desserts.addEventListener('click', function() {
     console.log('desserts');
 
     foodContainer.style.left =  '5%'
     foodContainerTitle.textContent = "Desserts";
+    theBody.classList.toggle("isActive")
+    theHamburgerButton.disabled = true;
 });
